630de3ba8265805c1ecedd58fdd03d92bcdcdb56
intae kim <intaemon@intaeui-MacBookAir.local>
2021-10-08 00:11:59 +0900

    chore: client 환경구성

---
+++ client/src/styles/GlobalStyles.jsx
@@ -0,0 +1,21 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { Global } from '@emotion/react';
+import tw, { css, GlobalStyles as BaseStyles } from 'twin.macro';
+
+const globalStyles = css`
+  body {
+    ${tw`bg-gray-900 text-white w-screen h-screen`}
+  }
+`;
+
+const GlobalStyles = () => {
+  return (
+    <>
+      <BaseStyles />
+      <Global styles={globalStyles} />
+    </>
+  );
+};
+
+export default GlobalStyles;

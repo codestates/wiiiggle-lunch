32a6914650b863438c363765537ca0c627db00de
Chung Seong Jun <77430059+jvn4dev@users.noreply.github.com>
2021-10-09 01:33:34 +0900

    new: 카드 공용 컴포넌트 구현

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import tw from 'twin.macro';
 
-import Select from './components/Create/Select';
+import Card from './components/shared/Card';
 
 export default function App() {
   return (
     <div css={tw`flex flex-col w-screen h-screen justify-center items-center`}>
-      <Select />
+      <Card />
     </div>
   );
 }

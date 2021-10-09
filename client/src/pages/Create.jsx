5cbced6e1226997acd46adad21005e579e6a4763
intae kim <intaemon@intaeui-MacBookAir.local>
2021-10-09 21:10:26 +0900

    new: Create page 레이아웃

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -1,3 +1,7 @@
+import Select from '@/components/Create/Select';
+import UploadBtn from '@/components/Create/UploadBtn';
+import Input from '@/components/elements/Input';
+import Label from '@/components/elements/Label';
 import Star from '@/components/shared/Star';
 import { useState } from 'react';
 import tw, { styled } from 'twin.macro';
@@ -8,12 +12,38 @@ export default function Create() {
     setScore(Number(e.currentTarget.dataset.value));
   };
   return (
-    <Layout>
-      <Star score={score} onClick={onClick} />
-      <Star lg score={score} onClick={onClick} />
-      <Star sm lock score={score} />
-    </Layout>
+    <Form>
+      <UploadBtn />
+      <Box>
+        <P>맛집 점수를 평가해주세요</P>
+        <Star score={score} onClick={onClick} />
+      </Box>
+      <Field>
+        <Label>
+          맛집 이름을 검색해주세요. (주소와 이름이 자동으로 등록됩니다.)
+        </Label>
+        <Select />
+      </Field>
+      <Field>
+        <Label>메뉴를 입력해주세요.</Label>
+        <Input underline placeholder="예: 돈까스" />
+      </Field>
+      <Field>
+        <Label>맛집 주소를 입력해주세요.</Label>
+        <Input underline placeholder="예: 서울 서초구 사평대로 56길 10" />
+      </Field>
+      <Field>
+        <Label>(옵션) TMI를 입력해주세요.</Label>
+        <Input
+          underline
+          placeholder="예: 매장 규모는 작으나, 직원분들이 친절합니다."
+        />
+      </Field>
+    </Form>
   );
 }
 
-const Layout = styled.div(() => [tw``]);
+const Form = styled.form(() => [tw`pt-2`]);
+const Field = styled.div(() => [tw`mb-8`]);
+const Box = styled.div(() => [tw`flex flex-col items-center my-8`]);
+const P = styled.p(() => [tw`text-xl mb-2`]);

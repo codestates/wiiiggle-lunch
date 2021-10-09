7d344abf86b66fdd3ec9a0a0077f183600deebc3
intae kim <intaemon@intaeui-MacBookAir.local>
2021-10-09 14:11:22 +0900

    new: Input, Label, Button 컴포넌트 구현

---
+++ client/src/components/elements/Input.jsx
@@ -0,0 +1,11 @@
+import tw, { styled } from 'twin.macro';
+
+const Input = styled.input(({ outline, underline, error, success }) => [
+  tw`w-full text-gray-700  px-1 focus:(outline-none border-yellow-300)`,
+  outline && tw`border-2 rounded-md py-2`,
+  underline && tw`border-b-2 border-gray-500 pb-2`,
+  error && tw`border-red-400`,
+  success && tw`border-green-400`,
+]);
+
+export default Input;

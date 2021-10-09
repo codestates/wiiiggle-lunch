ffb14e07eea00c9033958bc3e03e25ea45d47c56
yg kim <54833991+yg-kim-korean@users.noreply.github.com>
2021-10-09 12:07:46 +0900

    featrue/2-img-upload-check

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,6 +2,7 @@ module.exports = {
   login: require('./users/login'),
   signup: require('./users/signup'),
   posts: require('./posts/newposts'),
+  posts: require('./posts/posts'),
   accessTokenRequest: require('./users/accessTokenRequest'),
   refreshTokenRequest: require('./users/refreshTokenRequest'),
 };

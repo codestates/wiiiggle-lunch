c1e9f876cbf6c365e7119cacf876516d96f7bbd0
intae kim <intaemon@intaeui-MacBookAir.local>
2021-10-09 17:16:16 +0900

    new: Nav 컴포넌트 구현 & 아이콘 추가

--- client/src/components/shared/Nav.jsx
+++ client/src/components/shared/Nav.jsx
@@ -1,5 +1,11 @@
-import tw, { styled } from 'twin.macro';
+import tw, { styled, css } from 'twin.macro';
 import { useLocation } from 'react-router-dom';
+
+import { ReactComponent as Left } from 'assets/arrow-left.svg';
+import { ReactComponent as Search } from 'assets/search.svg';
+import { ReactComponent as Setting } from 'assets/cog.svg';
+import Button from '../elements/Button';
+import SearchBar from '../Search/SearchBar';
 
 const MAIN = 'main';
 const LOGIN = 'login';
@@ -19,49 +25,83 @@ export default function Nav() {
     case MYLIST:
       return (
         <StyledNav>
-          <NavItem>LOGIN 1</NavItem>
-          <NavItem>SIGNUP 2</NavItem>
-          <NavItem>MYLIST 3</NavItem>
+          <NavItem css={tw`justify-self-start`}>
+            <Left css={tw`w-8 h-8`} />
+          </NavItem>
+          <NavItem css={tw`uppercase tracking-wider font-bold text-lg`}>
+            {page}
+          </NavItem>
         </StyledNav>
       );
     case MAIN:
       return (
         <StyledNav>
-          <NavItem>MAIN 1</NavItem>
-          <NavItem>MAIN 2</NavItem>
-          <NavItem>MAIN 3</NavItem>
+          <NavItem
+            css={[
+              tw`uppercase tracking-wider font-bold text-primary justify-self-start`,
+              css`
+                grid-column: span 2;
+              `,
+            ]}
+          >
+            wiggle lunch
+          </NavItem>
+          <NavItem css={tw`justify-self-end`}>
+            <Search css={tw`w-6 h-6`} />
+          </NavItem>
         </StyledNav>
       );
     case MYPAGE:
       return (
         <StyledNav>
-          <NavItem>MYPAGE 1</NavItem>
-          <NavItem>MYPAGE 2</NavItem>
-          <NavItem>MYPAGE 3</NavItem>
+          <NavItem
+            css={tw`uppercase tracking-wider font-bold text-lg text-primary`}
+          >
+            {page}
+          </NavItem>
+          <NavItem />
+          <NavItem css={tw`justify-self-end`}>
+            <Setting css={tw`w-6 h-6`} />
+          </NavItem>
         </StyledNav>
       );
     case RESTAURANTS:
       return (
-        <StyledNav>
-          <NavItem>RESTAURANTS 1</NavItem>
-          <NavItem>RESTAURANTS 2</NavItem>
-          <NavItem>RESTAURANTS 3</NavItem>
+        <StyledNav css={tw`inline-flex border-none`}>
+          <NavItem css={tw`justify-self-start`}>
+            <Left css={tw`w-8 h-8`} />
+          </NavItem>
         </StyledNav>
       );
     case CREATE:
       return (
         <StyledNav>
-          <NavItem>CREATE 1</NavItem>
-          <NavItem>CREATE 2</NavItem>
-          <NavItem>CREATE 3</NavItem>
+          <NavItem css={tw`justify-self-start font-light`}>
+            <Button type="button" secondary text md>
+              닫기
+            </Button>
+          </NavItem>
+          <NavItem css={tw`font-bold text-gray-700`}>맛집 등록하기</NavItem>
+          <NavItem css={tw`justify-self-end`}>
+            <Button type="button" primary text md>
+              완료
+            </Button>
+          </NavItem>
         </StyledNav>
       );
     case SEARCH:
       return (
-        <StyledNav>
-          <NavItem>SEARCH 1</NavItem>
-          <NavItem>SEARCH 2</NavItem>
-          <NavItem>SEARCH 3</NavItem>
+        <StyledNav css={tw`border-none`}>
+          <NavItem css={tw`justify-self-start`}>
+            <Left css={tw`w-8 h-8`} />
+          </NavItem>
+          <NavItem
+            css={css`
+              grid-column: span 2;
+            `}
+          >
+            <SearchBar />
+          </NavItem>
         </StyledNav>
       );
     default:
@@ -69,5 +109,7 @@ export default function Nav() {
   }
 }
 
-const StyledNav = styled.nav(() => [tw``]);
+const StyledNav = styled.nav(() => [
+  tw`grid grid-cols-3 justify-items-center items-center h-14 border-b-2 border-gray-600 px-5`,
+]);
 const NavItem = styled.div(() => [tw``]);

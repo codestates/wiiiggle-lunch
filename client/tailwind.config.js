ac58ee793d8975c29a7520df9852de05eaa5608b
intae kim <intaemon@intaeui-MacBookAir.local>
2021-10-09 14:43:08 +0900

    chore: 테마 색상(primary) 추가

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        // eslint-disable-next-line
+        // prettier-ignore
+        'primary': '#ea4622',
+        'primary-light': '#ff7a4e',
+        'primary-dark': '#b00000',
+      },
     },
   },
   plugins: [],

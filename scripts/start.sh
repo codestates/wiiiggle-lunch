a6b90e2dea6fabbf9002350efe9c4316223ba10e
yg kim <54833991+yg-kim-korean@users.noreply.github.com>
2021-10-09 11:17:49 +0900

    feature/1 - 파일 삭제 및 작업환경 세팅

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,9 +6,10 @@ export DATABASE_USER=$(aws ssm get-parameters --region ap-northeast-2 --names DA
 export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PORT --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_NAME = $(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_NAME --query Parameters[0].Value | sed 's/"//g')
 # google env
-export OAUTH_CLIENT_ID=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
-export OAUTH_CLIENT_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
+export OAUTH_CLIENT_ID=$(aws ssm get-parameters --region ap-northeast-2 --names OAUTH_CLIENT_ID --query Parameters[0].Value | sed 's/"//g')
+export OAUTH_CLIENT_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names OAUTH_CLIENT_PASSWORD --query Parameters[0].Value | sed 's/"//g')
 
 # s3 env
 
---
+++ server/migrations/20211009011613-create-users-posts.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users_posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      users_id: {
+        type: Sequelize.INTEGER
+      },
+      posts_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users_posts');
+  }
+};

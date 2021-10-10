f8c5c6234bd36427ce95062c6e5588ce8b4c0496
Chung Seong Jun <77430059+jvn4dev@users.noreply.github.com>
2021-10-11 02:49:35 +0900

    New: MyList 페이지 구성

--- client/src/constants/cards.js
+++ client/src/constants/cards.js
@@ -1,6 +1,7 @@
 export const list = [
   {
     id: '1',
+    user: 'chung',
     name: '하이디라',
     menu: '훠궈',
     address: '서울 서초구 서초대로 77길 어쩌구 저쩌구',
@@ -10,6 +11,7 @@ export const list = [
   },
   {
     id: '2',
+    user: 'chung',
     name: '오스틴',
     menu: '퀘사디아',
     address: '서울 강남구 강남대로 10길 지하 1층',
@@ -19,6 +21,7 @@ export const list = [
   },
   {
     id: '3',
+    user: 'chung',
     name: '훌리오',
     menu: '피자',
     address: '서울 강남구 도산대로 18길 T-Tower 1층',
@@ -28,6 +31,7 @@ export const list = [
   },
   {
     id: '4',
+    user: 'kim',
     name: '우리 제육',
     menu: '제육볶음',
     address: '서울 서초구 서초대로 77길 어쩌구 저쩌구',
@@ -37,6 +41,7 @@ export const list = [
   },
   {
     id: '5',
+    user: 'kim',
     name: '애슐리',
     menu: '뷔페',
     address: '서울 서초구 서초대로 77길 어쩌구 저쩌구',
@@ -57,3 +62,38 @@ export const detailInfo = {
   lat: '127.02758590978978',
   lng: '37.5029617778497',
 };
+
+export const reviewLists = [
+  {
+    id: 1,
+    imgSrc:
+      'https://cdn.pixabay.com/photo/2017/09/28/18/13/bread-2796393_960_720.jpg',
+    name: '오스틴',
+    score: 3,
+    tmi: '매장이 깨끗해서 좋았다',
+  },
+  {
+    id: 2,
+    imgSrc:
+      'https://cdn.pixabay.com/photo/2017/09/28/18/13/bread-2796393_960_720.jpg',
+    name: '오스틴',
+    score: 4,
+    tmi: '매장이 깨끗해서 좋았다',
+  },
+  {
+    id: 3,
+    imgSrc:
+      'https://cdn.pixabay.com/photo/2017/09/28/18/13/bread-2796393_960_720.jpg',
+    name: '오스틴',
+    score: 2,
+    tmi: '매장이 깨끗해서 좋았다',
+  },
+  {
+    id: 4,
+    imgSrc:
+      'https://cdn.pixabay.com/photo/2017/09/28/18/13/bread-2796393_960_720.jpg',
+    name: '오스틴',
+    score: 5,
+    tmi: '매장이 깨끗해서 좋았다',
+  },
+];

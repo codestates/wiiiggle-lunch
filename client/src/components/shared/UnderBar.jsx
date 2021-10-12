895069de589cb8bce2be6ff5a80d3e94546e7bff
intae kim <intaemon@intaeui-MacBookAir.local>
2021-10-12 18:27:26 +0900

    improve: 언더바 컴포넌트가 현재 페이지를 표시합니다.

--- client/src/components/shared/UnderBar.jsx
+++ client/src/components/shared/UnderBar.jsx
@@ -7,21 +7,23 @@ import { ReactComponent as GridIcon } from 'assets/grid-add.svg';
 const MAIN = 'main';
 const MYPAGE = 'mypage';
 const MYLIST = 'mylist';
+const CREATE = 'create';
 
 export default function UnderBar() {
   const { pathname } = useLocation();
   const history = useHistory();
   const page = pathname.split('/')[1];
-  if (![MAIN, MYPAGE, MYLIST].includes(page)) return null;
+  console.log(page);
+  if (![MAIN, MYPAGE, MYLIST, CREATE].includes(page)) return null;
   return (
     <Container>
-      <Item onClick={() => history.push('/main')}>
+      <Item onClick={() => history.push('/main')} current={page === MAIN}>
         <HomeIcon css={tw`w-8 h-8`} />
       </Item>
-      <Item onClick={() => history.push('/create')}>
+      <Item onClick={() => history.push('/create')} current={page === CREATE}>
         <GridIcon css={tw`w-8 h-8`} />
       </Item>
-      <Item onClick={() => history.push('/mypage')}>
+      <Item onClick={() => history.push('/mypage')} current={page === MYPAGE}>
         <UserIcon css={tw`w-8 h-8`} />
       </Item>
     </Container>
@@ -31,4 +33,4 @@ export default function UnderBar() {
 const Container = styled.div(() => [
   tw`fixed left-0 right-0 bottom-0 grid grid-cols-3 place-items-center border-t-2 border-gray-700 h-14 bg-white`,
 ]);
-const Item = styled.div(() => [tw``]);
+const Item = styled.div(({ current }) => [current && tw`text-primary`, tw``]);
